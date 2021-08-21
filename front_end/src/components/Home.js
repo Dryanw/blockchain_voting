8bb27ba1f8269a3dc6107b000bdbeec7ca901d2a
Dryan <dryanwong@gmail.com>
2021-08-22 03:04:31 +0800

    Add icon

--- front_end/src/components/Home.js
+++ front_end/src/components/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import welcome from '../images/welcome.png';
 import QR from '../images/QR.png';
+import icon from '../images/icon.png'
 
 
 class Home extends React.Component {
     render() {
         return (
             <div style={{textAlign: "center"}}>
-                <h2 style={{textAlign: "left"}}>Welcome to Voltage</h2>
+                <h2 style={{textAlign: "left"}}>Welcome to Voltage   <img src={icon} style={{width: '48px'}}/></h2>
                 <img src={welcome} alt="Welcome"/><br/>
                 Source code on <a href="https://github.com/Dryanw/blockchain_voting">https://github.com/Dryanw/blockchain_voting</a><br/>
                 <img src={QR} alt="QR"/>
